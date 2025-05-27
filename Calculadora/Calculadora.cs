051f63fd6455705ef9e643832b3a496891d40d17
BautistaAlvarez <AlvarezBautista26@hotmail.com>
2025-05-27 13:44:13 -0300

    Segundo commit

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,4 +1,27 @@
-namespace EspacioCalculadora;
-class Calculadora{
-    float dato;
+namespace EspacioCalculadora{
+    public class Calculadora{
+        private double dato;
+
+        public Calculadora (double dato = 0){ //constructor ¿inicializar en 0?
+            this.dato = dato;
+        }
+        /*public Calculadora (double inicio = 0){
+            dato = inicio;
+        }*///esto es otra manera de inicializar, es lo mismo que el de arriba
+
+
+        public void sumar(double termino){
+            dato += termino;
+        }
+
+        public void dividir(double termino){
+            if (termino != 0) dato = dato/termino;
+            else Console.WriteLine("No se puede dividir en 0");
+        }
+
+        public double GetResultado(){
+            return dato;
+        }
+    }
+
 }
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EspacioCalculadora;
+
 Console.WriteLine("Hello, World!");
+
+Calculadora miCalculadora = new Calculadora(5);
+
+miCalculadora.sumar(23);
+miCalculadora.sumar(2);
+//uso de ejemplo
+miCalculadora.dividir(0);
+miCalculadora.dividir(5);
+
+Console.WriteLine($"El resultado es: {miCalculadora.GetResultado()}");
